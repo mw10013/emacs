d8496c72ada95a85d33e9c4d7085e0ea72cf14ba
Michael Wu <mw10013@gmail.com>
2012-10-09 23:02:46 -0400

    created

---
+++ init.el
@@ -0,0 +1,21 @@
+;; https://github.com/technomancy/swank-clojure
+;; 
+;; (setenv "PATH" (shell-command-to-string "echo $PATH"))
+
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(package-initialize)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ns-command-modifier (quote meta)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
